db89ba54849fcdda8f99c24ba25f952c34d195b8
Luke Hanna <luke.m.hanna@gmail.com>
2022-01-28 22:16:08 -0500

    Implemented JFX Window

--- src/main/java/com/pyredevelopment/pongonline/PongClient.java
+++ src/main/java/com/pyredevelopment/pongonline/PongClient.java
@@ -1,13 +1,31 @@
 package com.pyredevelopment.pongonline;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class PongClient extends Application {
 
+    public static void main(String[] args) {
+        launch();
+    }
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage)  {
 
+        stage.setTitle("Pong Connected Client");
+
+        VBox box = new VBox();
+        Canvas canvas = new Canvas(640, 480);
+        box.getChildren().add(canvas);
+
+        Scene scene = new Scene(box);
+
+
+        stage.setScene(scene);
+        stage.show();
     }
+
 }
