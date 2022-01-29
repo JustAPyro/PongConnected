ee9854de4894170ef28ec7b7a1d6861f2cb482b8
Luke Hanna <luke.m.hanna@gmail.com>
2022-01-29 00:26:15 -0500

    Added Datagram formatting and sending

--- src/main/java/com/pyredevelopment/pongonline/PongClient.java
+++ src/main/java/com/pyredevelopment/pongonline/PongClient.java
@@ -1,5 +1,6 @@
 package com.pyredevelopment.pongonline;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
@@ -7,11 +8,17 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.net.*;
+import java.util.BitSet;
 import java.util.HashSet;
 
 public class PongClient extends Application {
 
+    private DatagramSocket socket;
+    private InetAddress address;
     private final HashSet<KeyCode> keysPressed = new HashSet<>();
+
 
     public static void main(String[] args) {
         launch();
@@ -29,6 +36,33 @@ public class PongClient extends Application {
         Scene scene = new Scene(box);
         scene.setOnKeyPressed(e -> keysPressed.add(e.getCode()));
         scene.setOnKeyReleased(e -> keysPressed.remove(e.getCode()));
+
+        try {
+            socket = new DatagramSocket();
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+
+        AnimationTimer clock = new AnimationTimer() {
+            @Override
+            public void handle(long l) {
+                BitSet keyBits = new BitSet(8);
+                if (keysPressed.contains(KeyCode.W)) keyBits.set(0);
+                if (keysPressed.contains(KeyCode.S)) keyBits.set(1);
+
+                byte[] buf = keyBits.toByteArray();
+                System.out.println(buf.length);
+
+                try {
+
+                    DatagramPacket packet = new DatagramPacket(buf, buf.length, InetAddress.getLocalHost(), 4445);
+                    socket.send(packet);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        clock.start();
 
         stage.setScene(scene);
         stage.show();
